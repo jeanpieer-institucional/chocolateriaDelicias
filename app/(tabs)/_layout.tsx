14e9d262c068f1f41777029b5d12c89fbe6db7e7
Jean Pieer <jean.pinchi.ci@iestpantenororrego.edu.pe>
2025-12-01 03:05:00 -0500

    Se opmizo el diseño de la aplicación para mejor presentación inicial

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,28 +1,30 @@
 // app/_layout.tsx
-import { Tabs } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
 
 export default function Layout() {
-    return (
+  return (
     <Tabs
-    screenOptions={({ route }) => ({ 
-    tabBarIcon: ({ color, size }) => {
-    let iconName: string = 'home';
-    if (route.name === 'index') iconName = 'home';
-    else if (route.name === 'productos') iconName = 'cube';
-    else if (route.name === 'blogs') iconName = 'book';
-    else if (route.name === 'contacto') iconName = 'call';
-    return <Ionicons name={iconName as any} size={size} color={color}/>;
-    },
-    headerShown: false,
-    tabBarActiveTintColor: '#964B00',
-    tabBarInactiveTintColor: 'gray',
-    })}
->
-    <Tabs.Screen name="inicio" options={{ title: 'Inicio' }} />
-    <Tabs.Screen name="productos" options={{ title: 'Productos' }} />
-    <Tabs.Screen name="blogs" options={{ title: 'Blogs/Recetas' }} />
-    <Tabs.Screen name="contacto" options={{ title: 'Contacto' }} />
-  </Tabs>
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName: string = 'home';
+          if (route.name === 'index') iconName = 'home';
+          else if (route.name === 'productos') iconName = 'cube';
+          else if (route.name === 'blogs') iconName = 'book';
+          else if (route.name === 'contacto') iconName = 'call';
+          return <Ionicons name={iconName as any} size={size} color={color} />;
+        },
+        headerShown: false,
+        tabBarActiveTintColor: '#964B00',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      <Tabs.Screen name="inicio" options={{ title: 'Inicio' }} />
+      <Tabs.Screen name="productos" options={{ title: 'Productos' }} />
+      <Tabs.Screen name="blogs" options={{ title: 'Blogs/Recetas' }} />
+      <Tabs.Screen name="contacto" options={{ title: 'Contacto' }} />
+      <Tabs.Screen name="carrito" options={{ href: null }} />
+      <Tabs.Screen name="perfil" options={{ href: null }} />
+    </Tabs>
   );
 }
